90207e5ae770c3c70771549ecc97248a3796097a
José Elvis da Silva Souza Nogueira <elnogueira@MacBook-Pro-de-Jose.local>
2024-07-13 08:21:21 -0300

    initial commit

---
+++ src/main/kotlin/com/rinha_de_backend_2023/RinhaDeBackend2023Application.kt
@@ -0,0 +1,11 @@
+package com.rinha_de_backend_2023
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RinhaDeBackend2023Application
+
+fun main(args: Array<String>) {
+	runApplication<RinhaDeBackend2023Application>(*args)
+}
---
+++ src/test/kotlin/com/rinha_de_backend_2023/RinhaDeBackend2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rinha_de_backend_2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaDeBackend2023ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
